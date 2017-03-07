eb4b356dc3840c41c8087102af2a5dbd98d6c968
egrigokhan <egrigokhan@gmail.com>
2017-03-08 00:07:55 +0200

    Add files via upload

--- bundle.js
+++ bundle.js
@@ -6243,107 +6243,67 @@ function config (name) {
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],28:[function(require,module,exports){
-navigator.webkitGetUserMedia({video: true, audio: true}, function(stream) {
-		
-		//Creating peer
-		var Peer = require('simple-peer')
-		var peer = new Peer
-		({ 
-			initiator: location.hash === '', 
-			trickle: false, 
-			stream: stream
-		});
+var ownRTC;
+var validVideoIndex = 2;
 
-		//Variables
-		var validVideo = 1;
-		var ownID;
-		var userUID;
+var peerVideo;
+var selfVideo = document.getElementById( '1' );
 
-		//If error, report error
-		peer.on('error', function (err) { console.log('error', err) });
-		console.log("cool stuff");
+var url = window.location.search;
+var sessionKey = url.replace("?", '');
 
-		//If signaled, log your own data
-		peer.on('signal', function (data) {
-	  		console.log('SIGNAL', JSON.stringify(data)),
-	  		console.log("wick");
+var rootRef = firebase.database().ref('sessions/' + sessionKey);
+var newPeerRef = rootRef.push();
 
-	  		//Get own ID
-		  	ownID = JSON.stringify(data);
-		});
+navigator.getMedia = navigator.getUserMedia ||
+					 navigator.webkitGetUserMedia ||
+					 navigator.mozGetUserMedia ||
+					 navigator.msGetUserMedia;
 
-		console.log("cool stuff 2");
-
-		//Get the current sessionKey
-		var url = window.location.search;
-		var sessionKey = url.replace("?", '');
-		console.log(sessionKey);
-
-		// Initialize Firebase
-        //var config = 
-        //{
-		//	apiKey: "AIzaSyARfjKII2xxs01AAfBTEXkefDSEw1ctk3A",
-		//  authDomain: "bilsession.firebaseapp.com",
-		//    databaseURL: "https://bilsession.firebaseio.com",
-		//    storageBucket: "bilsession.appspot.com",
-		//   messagingSenderId: "399176089674"
-		//};
-		
-		//firebase.initializeApp(config);
-
-		console.log("success");
-
-	//Check if user is logged in
-	firebase.auth().onAuthStateChanged(function(user) {
-		if (user) 
-		{
-	    	console.log(user.uid);
-	    	var user = user;
-	    	userUID = user.uid;
-	    	console.log(userUID);
-	    	console.log("UP");
-	  	} else {
-	    // No user is signed in.
-	  }
-	});
-		
-		//Get base root ref for session databases
-		var rootRef = firebase.database().ref('sessions/' + sessionKey);
-		//var userRef = firebase.database().ref('sessions/' + sessionKey + '/' + userUID);
-		var newPeerRef = rootRef.push();
-		console.log("rat");
-
-		//On peer signal
-		peer.on('signal', function(data) {
-	  		
-	  		console.log("nice");
-	  		console.log(JSON.stringify(data));
-
-	  		newPeerRef.set(
-	  		{
-				//Save peer to database with current user ID
-				userUID : JSON.stringify(data)
-			}, function(error) {
-				console.log(error);
-			})	
-		});
-
-		//Reading values from database
-	    rootRef.on('value', function(snapshot) {
-	    snapshot.forEach(function(childSnapshot) {
-	  	var peerOther = JSON.parse(childSnapshot.val());
-	  	peer.signal(peerOther);
-	  });
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+	    console.log('USER', user.uid);
+	   	userUID = user.uid;
+	  	} else {}
 });
-	  peer.on('stream', function(stream) {
-	  	var video = document.getElementById('' + validVideo);
-	  	validVideo++;
-	  	video.src = window.URL.createObjectURL(stream);
-	  	video.play();
-	  });
 
+navigator.getMedia({ video: true, audio: false }, function( stream ) {
+	//selfVideo.setAtribute( 'src' ) = vendorUrl.createObjectURL( stream );
 
-  }, function(error) {
-  	console.log(error);
-  });
+	var Peer = require( 'simple-peer' );
+	var peer = new Peer(
+	{
+		initiator: location.hash === '',
+		trickle: false,
+		stream: stream
+	});
+
+	peer.on( 'signal', function( data ) {
+		console.log('USER RTC ID: ', JSON.stringify( data ) );
+		ownRTC = JSON.stringify( data );
+		newPeerRef.set(
+		{
+			userUID : ownRTC
+		}
+		);
+	});
+
+	rootRef.on( 'value', function( snapshot ) {
+		snapshot.forEach( function( childSnapshot ) {
+			if( childSnapshot.val() != ownRTC ) {
+				peer.signal( JSON.parse( childSnapshot.val() ) );
+			}
+		});
+	});
+
+	peer.on( 'stream', function( stream ) {
+		peerVideo = document.getElementById( '' + validVideoIndex );
+		//peerVideo.setAtribute( 'src' ) = vendorUrl.createObjectURL( stream );
+		peerVideo.play();
+	}, function( error ) {
+	console.log( 'PEER STREAM ERROR: ', error );
+	});
+}, function( error ) {
+	console.log( 'SELF STREAM ERROR: ', error );
+} );
 },{"simple-peer":25}]},{},[28]);
